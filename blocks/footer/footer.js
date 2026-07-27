3ea60b40510899b9fd5fff823a91a57b31977860
Yoge93 <yogesh.walwalkar@deptagency.com>
2026-07-27 22:02:18 +0530

    header handling v2

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -40,7 +40,9 @@ export default async function decorate(block) {
   // decorate footer DOM
   block.textContent = '';
   const footer = document.createElement('div');
-  while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
-
-  block.append(footer);
+  if(fragment){
+    while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
+  
+    block.append(footer);
+  }
 }
